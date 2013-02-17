77d4386193621026327fee7ea26059b165712ebc
Diego Ramos <rdiego26@gmail.com>
2013-02-16 22:04:03 -0200

    Commit

---
+++ src/info/diegoramos/healthanywhere/database/Tabelas.java
@@ -0,0 +1,27 @@
+package info.diegoramos.healthanywhere.database;
+
+public class Tabelas {
+
+	//Tabela cad_serie
+	
+	
+	//Tabela cad_programa
+	private static final String TBL_cad_programa = "cad_programa";
+		//CAMPOS cad_programa	
+		private static final String FIELD_id_programa = "_id_programa INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT";
+		private static final String FIELD_nome_programa = "nome CHAR(100) NOT NULL CONSTRAINT [nome_programa] UNIQUE";
+		private static final String FIELD_objetivo_programa = "objetivo CHAR(100)";
+		
+		//DDL tabela cad_programa
+		private String ddl_cad_programa = "CREATE TABLE " +
+			TBL_cad_programa + " (" +
+			FIELD_id_programa + " , " + 
+			FIELD_nome_programa + " , " +
+			FIELD_objetivo_programa + " , " +
+			")";
+	
+	
+	//Tabela cad_serie_exercicio
+	
+	
+}
---
+++ src/info/diegoramos/healthanywhere/model/Programa.java
@@ -0,0 +1,46 @@
+package info.diegoramos.healthanywhere.model;
+
+public class Programa {
+	private Long id_programa;
+	private String nome;
+	private String objetivo;
+	
+	
+	/* 
+	 * @author Diego Ramos
+	 * @description Classe da Entidade Programa
+	 */
+	
+	
+	/* 
+	 * Getters and Setters
+	 * 
+	 */	
+	
+	public Long getId_programa() {
+		return id_programa;
+	}
+	
+	public void setId_programa(Long id_programa) {
+		this.id_programa = id_programa;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getObjetivo() {
+		return objetivo;
+	}
+
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+	
+	
+	
+}
